dcfa65e122874dcf90939c5af9e41971fec8be39
Stefan Arentz <stefan@arentz.ca>
2013-05-21 19:49:22 +0000

    Find database in home directory

--- minion/frontend/__init__.py
+++ minion/frontend/__init__.py
@@ -2,11 +2,13 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import os
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__);
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/stefan/minion-frontend-new.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.expanduser("~/minion-frontend.db")
 db = SQLAlchemy(app)
 
 from minion.frontend import views, models
