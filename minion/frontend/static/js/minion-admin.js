f57b7075836dfba7be6eec2d2bd31f542a2a15e6
Stefan Arentz <stefan@arentz.ca>
2013-06-13 16:44:17 -0400

    Fixes #11 Adding sites and users to a group should not allow freeform entry

--- minion/frontend/static/js/minion-admin.js
+++ minion/frontend/static/js/minion-admin.js
@@ -124,75 +124,82 @@ app.controller("AdminGroupsController", function($scope, $routeParams, $http, $l
 
 // Controllers for /groups/:groupName
 
-app.controller("AdminAddUserController", function ($scope, dialog) {
-    $scope.close = function(site){
-        dialog.close(site);
+app.controller("AdminAddUserController", function ($scope, dialog, users) {
+    $scope.users = users;
+    $scope.cancel = function() {
+        dialog.close(null);
+    };
+    $scope.submit = function(email){
+        dialog.close(email);
     };
 });
 
-app.controller("AdminAddSiteController", function ($scope, dialog) {
-    $scope.close = function(site){
+app.controller("AdminAddSiteController", function ($scope, dialog, sites) {
+    $scope.sites = sites;
+    $scope.submit = function(site){
         dialog.close(site);
+    };
+    $scope.cancel = function(site){
+        dialog.close(null);
     };
 });
 
 app.controller("AdminGroupController", function($scope, $routeParams, $http, $location, $dialog) {
     var reload = function () {
-        $http.get('/api/admin/groups/' + $routeParams.groupName)
-            .success(function(response, status, headers, config) {
-                $scope.group = response.data;
-            });
+        $http.get('/api/admin/groups/' + $routeParams.groupName).success(function(response) {
+            $scope.group = response.data;
+        });
     };
 
     $scope.addUser = function() {
-        $dialog.dialog({templateUrl: "static/partials/admin/add-user-dialog.html?x=1", controller: "AdminAddUserController" })
-            .open().then(function(email) {
-                if (email) {
-                    var patch = {addUsers:[email]};
+        $http.get("/api/admin/users").success(function(response) {
+            // Remove the sites already in the group. Sort them.
+            var users = _.filter(response.data, function (user) { return !_.contains($scope.group.users, user.email); });
+            // Show the dialog
+            var r = { users: function () { return users; } };
+            $dialog.dialog({templateUrl: "static/partials/admin/add-user-dialog.html?" + new Date().getTime(), controller: "AdminAddUserController", resolve: r }).open().then(function(user) {
+                if (user) {
+                    var patch = {addUsers:[user.email]};
                     var url = '/api/admin/groups/' + $routeParams.groupName;
-                    $http({method:'PATCH', url:url, headers: {"Content-Type": "application/json"}, data:patch})
-                        .success(function(response, status, headers, config) {
-                            if (response.success) {
-                                reload();
-                            }
-                        });
+                    $http({method:'PATCH', url:url, headers: {"Content-Type": "application/json"}, data:patch}).success(function(response) {
+                        reload();
+                    });
                 }
             });
+        });
     };
 
     $scope.removeUser = function(email) {
         var title = 'Delete User';
         var msg = 'Are you sure you want to remove ' + email + ' from the group?';
         var btns = [{result:false, label: 'Cancel'}, {result:true, label: 'OK', cssClass: 'btn-primary'}];
-        $dialog.messageBox(title, msg, btns).open().then(function(result){
+        $dialog.messageBox(title, msg, btns).open().then(function(result) {
             if (result) {
                 var patch = {removeUsers:[email]};
                 var url = '/api/admin/groups/' + $routeParams.groupName;
-                $http({method:'PATCH', url:url, headers: {"Content-Type": "application/json"}, data:patch})
-                    .success(function(response, status, headers, config) {
-                        if (response.success) {
-                            reload();
-                        }
-                    });
+                $http({method:'PATCH', url:url, headers: {"Content-Type": "application/json"}, data:patch}).success(function(response) {
+                        reload();
+                });
             }
         });
     };
 
     $scope.addSite = function() {
-        console.log("ADD SITE");
-        $dialog.dialog({templateUrl: "static/partials/admin/add-site-dialog.html?x=1", controller: "AdminAddSiteController" })
-            .open().then(function(url) {
-                if (url) {
-                    var patch = {addSites:[url]};
-                    var api = '/api/admin/groups/' + $routeParams.groupName;
-                    $http({method:'PATCH', url:api, headers: {"Content-Type": "application/json"}, data:patch})
-                        .success(function(response, status, headers, config) {
-                            if (response.success) {
-                                reload();
-                            }
-                        });
+        $http.get("/api/admin/sites").success(function(response) {
+            // Remove the sites already in the group. Sort them.
+            var sites = _.filter(response.data, function (site) { return !_.contains($scope.group.sites, site.url); });
+            // Show the dialog
+            var r = { sites: function () { return sites; } };
+            $dialog.dialog({templateUrl: "static/partials/admin/add-site-dialog.html?" + new Date().getTime(), controller: "AdminAddSiteController", resolve: r }).open().then(function(site) {
+                if (site) {
+                    var patch = {addSites:[site.url]};
+                    var url = '/api/admin/groups/' + $routeParams.groupName;
+                    $http({method:'PATCH', url:url, headers: {"Content-Type": "application/json"}, data:patch}).success(function(response) {
+                        reload();
+                    });
                 }
             });
+        });
     };
 
     $scope.removeSite = function(site) {
